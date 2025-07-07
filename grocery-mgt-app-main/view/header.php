36cf3674bad43094c51e23cae3dca9e44a3abe0b
zkahler <zach.kahler@southeasttech.edu>
2025-07-07 01:46:55 -0500

    Update header.php

--- grocery-mgt-app-main/view/header.php
+++ grocery-mgt-app-main/view/header.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Manage your grocery list efficiently with this grocery management system.">
     <title>Grocery Dashboard</title>
-    <link rel="stylesheet" href="/Unit2Project/grocery-mgt-app-main/css/style.css">
+    <link rel="stylesheet" href="/grocery-mgt-app-main/css/style.css">
 </head>
 <body>
     <header>
